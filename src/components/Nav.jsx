39ea74cf7762aada6c590191e23e602615389455
rjcodebytes <136474535+rjcodebytes@users.noreply.github.com>
2024-09-12 17:04:35 +0530

    initialization

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,13 +2,22 @@ import React from 'react'
 import Image from 'next/image'
 import logo from "../../public/logo.png"
 import Link from 'next/link'
+import { Roboto } from 'next/font/google'
+ 
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
 export default function Nav() {
   return (
-    <nav className='w-screen lg:h-20 bg-white px-10 py-2 shadow-md'>
+    <nav className='w-screen flex justify-between lg:h-16 bg-white px-10 items-center shadow-md'>
         <div className="logo w-[115px] h-[62px]">
            <Link href=""><Image src={logo} /></Link>
         </div>
-        <Link href={""}>Join</Link>
+        <Link href={""} className={`bg-[#6E76E5] text-white w-24 h-9 p-2 flex justify-center items-center rounded-[40px] ${roboto.className}`}>Join</Link>
+        
     </nav>
   )
 }
