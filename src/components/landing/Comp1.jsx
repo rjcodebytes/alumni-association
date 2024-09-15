54bdb85289293917b2e40704f92e84371e4b1c59
rjcodebytes <136474535+rjcodebytes@users.noreply.github.com>
2024-09-15 14:41:41 +0530

    ui

--- src/components/landing/Comp1.jsx
+++ src/components/landing/Comp1.jsx
@@ -8,9 +8,13 @@ const poppins = Poppins({
 export default function Comp1() {
     return (
         <div className='w-full mx-auto flex flex-col '>
-            <div className='items-start px-1 lg:px-11 py-2 lg:py-6'>
+            <div className='mx-auto lg:mx-0 items-start px-1 lg:px-11 py-2 lg:py-6'>
                 <div className='w-full text-gray-400'>
-                    <p className={`${poppins.className} text-[26px] md:text-[60px] lg:text-[60px] font-semibold`}>Reunite,Engage,</p>
+                    <p className={`${poppins.className} text-[31px] md:text-[60px] lg:text-[60px] font-semibold`}>Reunite,Engage,</p>
+                </div>
+                <div className='flex justify-start items-center w-full text-gray-400'>
+                    <p className={`${poppins.className} text-[31px] md:text-[60px] lg:text-[60px] font-semibold`}>Excel,</p>
+                    <p className={`${poppins.className} text-[31px] md:text-[60px] lg:text-[60px] font-semibold bg-gradient-to-r text-transparent from-[#2629B4] to-[#AA27AA] bg-clip-text`}>Together</p>
                 </div>
             </div>
             <div className='py-3 mx-auto w-full h-fit flex-col lg:flex-row md:flex-row shadow-lg md:h-[150px] md:w-[1000px] lg:h-[150px] lg:w-[1000px] rounded-[30px] bg-[#FBF9F9] flex justify-around items-center'>
